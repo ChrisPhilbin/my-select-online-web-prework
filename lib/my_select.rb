d4995baa4a07fcfb93eee2620acf9cd77bcd5b84
fortunate-database-4943 <chris@chrisphilbin.net>
2018-11-17 22:17:50 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
- # your code here!
+  i = 0
+  while i < collection.length
+    yield(collection[i])
+    i = i + 1
+  end
+end
+
+my_select([1, 2, 3, 4, 5]) do |num|
+  num.even?
 end
