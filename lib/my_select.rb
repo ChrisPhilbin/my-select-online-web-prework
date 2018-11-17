4a98afb7ea6aedd1d668c425d7dba360901069f5
fortunate-database-4943 <chris@chrisphilbin.net>
2018-11-17 22:18:21 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,7 +1,8 @@
 def my_select(collection)
   i = 0
+  new_collection = []
   while i < collection.length
-    yield(collection[i])
+    new_collection << yield(collection[i])
     i = i + 1
   end
 end
