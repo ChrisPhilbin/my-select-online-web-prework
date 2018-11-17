2a8388a6346de6485b0f5b5241ac5725ad742f6d
fortunate-database-4943 <chris@chrisphilbin.net>
2018-11-17 22:39:37 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,14 +2,9 @@ def my_select(collection)
   i = 0
   new_collection = []
   while i < collection.length
-    new_collection << yield(collection[i])
-    i = i + 1
+    if collection[i] % 2 == 0
+      new_collection << yield(collection[i])
+      i = i + 1
   end
   new_collection
-end
-
-my_select([1, 2, 3, 4, 5]) do |num|
-  if num % 2 == 0
-    num
-  end
 end
